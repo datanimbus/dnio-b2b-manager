b2e552706fb73fae6af4405cb182a9e995c2c39a
Jugnu Agrawal <jugnu@appveen.com>
2022-02-07 11:37:10 +0530

    code-gen folder

--- code-gen/copy-faas.js
+++ code-gen/copy-faas.js
@@ -3,7 +3,7 @@ const path = require('path');
 const copy = require('recursive-copy');
 
 const dataStackFunctions = path.join(process.cwd(), '../ds-faas/generator');
-const generatorsFolder = path.join(process.cwd(), 'codeGen/faas/generators');
+const generatorsFolder = path.join(process.cwd(), 'code-gen/faas/generators');
 
 let indexContent = fs.readFileSync(path.join(dataStackFunctions, 'index.js'), 'utf-8');
 
@@ -12,6 +12,6 @@ if (fs.existsSync(generatorsFolder)) {
 }
 
 copy(path.join(dataStackFunctions, 'generators'), generatorsFolder).then(files => {
-    fs.writeFileSync('./codeGen/faas/index.js', indexContent, 'utf-8');
+    fs.writeFileSync('./code-gen/faas/index.js', indexContent, 'utf-8');
 	console.log('Copied', files.length);
 }).catch(console.error);
