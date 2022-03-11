272ba6b3ee06a06cffd459d9d09ff525b081c802
Jugnu Agrawal <jugnu@appveen.com>
2022-03-12 00:09:35 +0530

    fetchAll faas

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,29 @@
 const router = require('express').Router();
+const log4js = require('log4js');
+const mongoose = require('mongoose');
 
+const queryUtils = require('../utils/query.utils');
 
+const logger = log4js.getLogger('index.controller');
+const faasModel = mongoose.model('faas');
+
+router.get('/faas/fetchAll', (req, res) => {
+	try {
+		const filter = queryUtils.parseFilter(req.query.filter);
+		if (req.query.countOnly) {
+			const count = await faasModel.countDocuments(filter);
+			return res.status(200).json(count);
+		}
+		const data = queryUtils.getPaginationData(req);
+		const docs = await faasModel.find(filter).select(data.select).sort(data.sort).skip(data.skip).limit(data.count).lean();
+		res.status(200).json(docs);
+	} catch (err) {
+		logger.error(err);
+		res.status(500).json({
+			message: err.message
+		});
+	}
+});
 router.use('/app', require('./app.controller'));
 // router.use('/:app/agent', paramParser, require('./agent.controller'));
 // router.use('/:app/agentRegistry', paramParser, require('./agent-registry.controller'));
