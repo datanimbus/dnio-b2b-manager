064ff719cc1fd28a1cfa7b347e3ff5ab5b691ddf
Jugnu Agrawal <jugnu@appveen.com>
2022-03-16 17:37:59 +0530

    local route map

--- utils/router.utils.js
+++ utils/router.utils.js
@@ -1,6 +1,8 @@
 const log4js = require('log4js');
 const mongoose = require('mongoose');
 
+const config = require('../config');
+
 const logger = log4js.getLogger(global.loggerName);
 const flowModal = mongoose.model('flow');
 
@@ -9,7 +11,11 @@ async function initRouterMap() {
 		const flows = await flowModal.find({}).lean();
 		global.activeFlows = {};
 		flows.forEach(item => {
-			global.activeFlows['/' + item.app + item.inputStage.options.path] = `http://${item.deploymentName}.${item.namespace}`;
+			if (config.isK8sEnv()) {
+				global.activeFlows['/' + item.app + item.inputStage.options.path] = `http://${item.deploymentName}.${item.namespace}`;
+			} else {
+				global.activeFlows['/' + item.app + item.inputStage.options.path] = `http://localhost:${item.port || 31000}`;
+			}
 		});
 		logger.trace(global.activeFlows);
 	} catch (err) {
