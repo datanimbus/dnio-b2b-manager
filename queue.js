78604baa69bfef187153a807bc7cd39fcbbc52e9
kavi <kavi@appveen.com>
2022-05-06 17:05:27 +0530

    data fix

--- queue.js
+++ queue.js
@@ -58,39 +58,11 @@ function faasInvokeLogger() {
 		const faasModel = mongoose.model('faas');
 		let bodyObj = JSON.parse(_body.getData());
 		logger.trace(`Message from queue :: ${config.faasLastInvokedQueue} :: ${JSON.stringify(bodyObj)}`);
-		const payload = bodyObj.data;
 		try {
-			await faasModel.findOneAndUpdate({ "_id": payload._id }, { $set: { "lastInvokedAt": new Date(payload.startTime) }});
+			await faasModel.findOneAndUpdate({ "_id": bodyObj._id }, { $set: { "lastInvokedAt": new Date(bodyObj.startTime) }});
 		} catch (err) {
 			logger.error('Error updating function lastInvokedTime :: ', err);
 		}
-		
-		
-		
-		// let colName = bodyObj.collectionName;
-		// let colName = 'b2b.faas.logs';
-		
-		// let mongoDBColl = mongoose.connection.db.collection(colName);
-		// if (colName && payload) {
-		// 	payload.colName = bodyObj.collectionName;
-		// 	fixAPILogPayload(payload);
-		// 	fixMetaData(payload);
-		// 	let promise = Promise.resolve();
-		// 	if (!payload.serviceId) {
-		// 		promise = serviceUtils.getServiceId(bodyObj.collectionName);
-		// 	}
-		// 	promise.then(data => {
-		// 		if (data && data._id) {
-		// 			payload.serviceId = data._id;
-		// 		}
-		// 		return mongoDBColl.insert(payload)
-		// 			.catch(err => {
-		// 				logger.error(err);
-		// 			});
-		// 	}).catch(err => {
-		// 		logger.error(err);
-		// 	});
-		// }
 	});
 }
 
