f1ff61796a09d08de9c42e4af2b404ee32a9a6bb
Jugnu Agrawal <jugnu@appveen.com>
2022-09-30 12:28:23 +0530

    id generation

--- models/interaction.model.js
+++ models/interaction.model.js
@@ -13,5 +13,6 @@ const mongooseUtils = require('../utils/mongoose.utils');
 const schema = mongooseUtils.MakeSchema(definition);
 
 schema.plugin(mongooseUtils.metadataPlugin());
+schema.pre('save', mongooseUtils.generateId('INTR', 'interations', null, 4, 1000));
 
 mongoose.model('interaction', schema, 'b2b.interactions');
