bc5cddad4afd915b671cb30971014878caf14010
Jugnu Agrawal <jugnu@appveen.com>
2022-10-13 01:00:28 +0530

    commented for testing

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,44 +4,47 @@ set -e
 
 TAG=`cat CURRENT_BM`
 
-echo "****************************************************"
-echo "data.stack:bm :: Building B2B Base using TAG :: $TAG"
-echo "****************************************************"
+# echo "****************************************************"
+# echo "data.stack:bm :: Building B2B Base using TAG :: $TAG"
+# echo "****************************************************"
 
-cd $WORKSPACE/ds-b2b-base
+# cd $WORKSPACE/ds-b2b-base
 
-sed -i.bak s#__image_tag__#$TAG# Dockerfile
+# sed -i.bak s#__image_tag__#$TAG# Dockerfile
 
-if [ $cleanBuild ]; then
-    docker build --no-cache -t data.stack.b2b.base:$TAG .
-else 
-    docker build -t data.stack.b2b.base:$TAG .
-fi
-
-
-echo "****************************************************"
-echo "data.stack:bm :: B2B Base Built using TAG :: $TAG"
-echo "****************************************************"
-
-echo "****************************************************"
-echo "data.stack:bm :: Building FaaS Base using TAG :: $TAG"
-echo "****************************************************"
-
-cd $WORKSPACE/ds-faas
-
-sed -i.bak s#__image_tag__#$TAG# Dockerfile
-
-if [ $cleanBuild ]; then
-    docker build --no-cache -t data.stack.faas.base:$TAG .
-else 
-    docker build -t data.stack.faas.base:$TAG .
-fi
+# if [ $cleanBuild ]; then
+#     docker build --no-cache -t data.stack.b2b.base:$TAG .
+# else 
+#     docker build -t data.stack.b2b.base:$TAG .
+# fi
 
 
-echo "****************************************************"
-echo "data.stack:bm :: FaaS Base Built using TAG :: $TAG"
-echo "****************************************************"
+# echo "****************************************************"
+# echo "data.stack:bm :: B2B Base Built using TAG :: $TAG"
+# echo "****************************************************"
 
+# echo "****************************************************"
+# echo "data.stack:bm :: Building FaaS Base using TAG :: $TAG"
+# echo "****************************************************"
+
+# cd $WORKSPACE/ds-faas
+
+# sed -i.bak s#__image_tag__#$TAG# Dockerfile
+
+# if [ $cleanBuild ]; then
+#     docker build --no-cache -t data.stack.faas.base:$TAG .
+# else 
+#     docker build -t data.stack.faas.base:$TAG .
+# fi
+
+
+# echo "****************************************************"
+# echo "data.stack:bm :: FaaS Base Built using TAG :: $TAG"
+# echo "****************************************************"
+
+
+echo "$buildAgent"
+echo "$buildAgentWatcher"
 
 if [ "$buildAgent"==true ]; then
     echo "****************************************************"
