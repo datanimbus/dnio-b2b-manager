7a257533812007fcce4c60fb70614cea8624a38c
kavi-appveen <kavi@appveen.com>
2023-08-16 12:35:03 +0530

    DS-611

--- controllers/faas.utils.controller.js
+++ controllers/faas.utils.controller.js
@@ -190,7 +190,7 @@ router.put('/:id/deploy', async (req, res) => {
 			const status = await k8sUtils.upsertFaasDeployment(doc);
 			logger.debug(`Upsert service status :: ${service.statusCode}`);
 			logger.debug(`Upsert deployment status :: ${status.statusCode}`);
-			if ((status.statusCode !== 200 && status.statusCode !== 202) || (service.statusCode !== 200 && service.statusCode !== 202)) {
+			if ((status.statusCode <= 200 || status.statusCode >= 202) || (service.statusCode <= 200 || service.statusCode >= 202)) {
 				return res.status(status.statusCode).json({ message: 'Unable to deploy function' });
 			}
 		}
