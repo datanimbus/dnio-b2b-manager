9c6e2b74819d4bb5a9edccc5eda641eabbd4ca1b
sushmithaAppVeen <sushmitha@appveen.com>
2023-09-12 00:09:58 +0530

    Added ttl for agent logs

--- models/agent-logs.model.js
+++ models/agent-logs.model.js
@@ -4,8 +4,10 @@ const mongoose = require('mongoose');
 const definition = require('../schemas/agent-logs.schema').definition;
 const mongooseUtils = require('../utils/mongoose.utils');
 
+const expiry = config.B2B_AGENT_LOGS_TTL_DAYS * 86400;
 const schema = mongooseUtils.MakeSchema(definition);
 schema.plugin(mongooseUtils.metadataPlugin());
 schema.index({ app: 1, agentId: 1 });
+schema.index({ timestamp: 1 }, { expireAfterSeconds: expiry });
 
 mongoose.model('agent-logs', schema, 'b2b.agent.logs');
