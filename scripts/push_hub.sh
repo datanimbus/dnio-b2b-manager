e8ab1ffa1f027992b0aaf6f0986fb905628398e5
Jugnu Agrawal <jugnu@appveen.com>
2023-09-20 14:53:49 +0530

    dockerhub change

--- scripts/push_hub.sh
+++ scripts/push_hub.sh
@@ -5,26 +5,26 @@ set -e
 TAG=`cat CURRENT_BM`
 
 echo "****************************************************"
-echo "datanimbus.io.bm :: Pushing Image to Docker Hub :: appveen/datanimbus.io.bm:$TAG"
+echo "datanimbus.io.bm :: Pushing Image to Docker Hub :: datanimbus/datanimbus.io.bm:$TAG"
 echo "****************************************************"
 
-docker tag datanimbus.io.bm:$TAG appveen/datanimbus.io.bm:$TAG
-docker push appveen/datanimbus.io.bm:$TAG
+docker tag datanimbus.io.bm:$TAG datanimbus/datanimbus.io.bm:$TAG
+docker push datanimbus/datanimbus.io.bm:$TAG
 
 echo "****************************************************"
-echo "datanimbus.io.bm :: Image Pushed to Docker Hub AS appveen/datanimbus.io.bm:$TAG"
+echo "datanimbus.io.bm :: Image Pushed to Docker Hub AS datanimbus/datanimbus.io.bm:$TAG"
 echo "****************************************************"
 
-docker tag datanimbus.io.b2b.base:$TAG appveen/datanimbus.io.b2b.base:$TAG
-docker push appveen/datanimbus.io.b2b.base:$TAG
+docker tag datanimbus.io.b2b.base:$TAG datanimbus/datanimbus.io.b2b.base:$TAG
+docker push datanimbus/datanimbus.io.b2b.base:$TAG
 
 echo "****************************************************"
-echo "datanimbus.io.bm :: Image Pushed to Docker Hub AS appveen/datanimbus.io.b2b.base:$TAG"
+echo "datanimbus.io.bm :: Image Pushed to Docker Hub AS datanimbus/datanimbus.io.b2b.base:$TAG"
 echo "****************************************************"
 
-docker tag datanimbus.io.faas.base:$TAG appveen/datanimbus.io.faas.base:$TAG
-docker push appveen/datanimbus.io.faas.base:$TAG
+docker tag datanimbus.io.faas.base:$TAG datanimbus/datanimbus.io.faas.base:$TAG
+docker push datanimbus/datanimbus.io.faas.base:$TAG
 
 echo "****************************************************"
-echo "datanimbus.io.bm :: Image Pushed to Docker Hub AS appveen/datanimbus.io.faas.base:$TAG"
+echo "datanimbus.io.bm :: Image Pushed to Docker Hub AS datanimbus/datanimbus.io.faas.base:$TAG"
 echo "****************************************************"
