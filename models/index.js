374aa111b66bc35772f2316378c2c8f16f08583b
Jugnu Agrawal <jugnu@appveen.com>
2024-01-05 16:35:20 +0530

    model fix and cleanup

--- models/index.js
+++ models/index.js
@@ -2,10 +2,11 @@ function init(){
 	require('./agent-action.model');
 	require('./agent-logs.model');
 	require('./agent.model');
+	require('./bundle-deployment.model');
+	require('./custom-node.model');
 	require('./data-format.model');
 	require('./faas.model');
 	require('./flow.model');
 	require('./interaction.model');
-	require('./custom-node.model');
 }
 module.exports.init = init;
